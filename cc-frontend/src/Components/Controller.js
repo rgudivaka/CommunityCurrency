8122b24b5b92392a62fe7055b5a84a9f49afbb55
rgudivaka <rishi.gudivaka@gmail.com>
2020-06-10 19:48:23 -0500

    final changes

--- cc-frontend/src/Components/Controller.js
+++ cc-frontend/src/Components/Controller.js
@@ -1,32 +1,27 @@
-import React, { useState } from 'react';
-import Login from './Login'
-import Settings from './Settings'
-import Dashboard from './Dashboard';
-import CollectionBoxesPage from './CollectionBoxesPage';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom"
+import React, { useState } from "react";
+import Login from "./Login";
+import Settings from "./Settings";
+import Dashboard from "./Dashboard";
+import CollectionBoxesPage from "./CollectionBoxesPage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-export default function Controller(props){
-    return (
-        <Router forceRefresh={true}>
-            <Switch>
-                <Route path="/dashboard">
-                    <Dashboard />
-                </Route>
-                <Route path="/settings">
-                    <Settings />
-                <Route path="/boxes">
-                    <CollectionBoxesPage api={props.api}/>
-                </Route>
-                <Route path="/">
-                    <Login />
-                </Route>
-
-            </Switch>
-        </Router>
-    );
+export default function Controller(props) {
+  return (
+    <Router forceRefresh={true}>
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/settings">
+          <Settings />
+        </Route>
+        <Route path="/boxes">
+          <CollectionBoxesPage api={props.api} />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
--- cc-frontend/src/Components/Dashboard.js
+++ cc-frontend/src/Components/Dashboard.js
@@ -14,7 +14,7 @@ import Badge from "@material-ui/core/Badge";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import NotificationsIcon from "@material-ui/icons/Notifications";
@@ -23,23 +23,6 @@ import Chart from "./Chart";
 import Deposits from "./Deposits";
 import Orders from "./Orders";
 import Copyright from "./Copyright";
-
-<<<<<<< HEAD
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://www.commcurrency.com/">
-        Community Currency
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-=======
->>>>>>> master
 
 const drawerWidth = 240;
 
